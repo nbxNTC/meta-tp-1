f1c7dfafa4295928b82aead0b89110a6ff88347b
Mario Sergio <marionbxcz@gmail.com>
2021-09-04 23:33:17 -0300

    feat: create hill-climbing script

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM python:3.8
-
-COPY script.py /code/
-
-CMD ["python", "/code/script.py"]
---
+++ hill-climbing/script.py
@@ -0,0 +1,39 @@
+import random
+import math
+
+def generateRandomValue(minValue, maxValue):
+  return random.uniform(minValue, maxValue)
+
+def generateRandomSolution(minArray, maxArray):
+  return [generateRandomValue(minArray[0], maxArray[0]), generateRandomValue(minArray[1], maxArray[1])]
+
+def calculateSolutionValue(solution):
+  x = solution[0]
+  y = solution[1]
+  return math.sin(x + y) + math.pow((x - y), 2) - (1.5 * x) + (2.5 * y) + 1
+
+def printSolution(title, solution):
+  print(title)
+  print('-----------------------')
+  print('x = ', solution[0])
+  print('y =', solution[1])
+  print('Result =', calculateSolutionValue(solution), '\n')
+
+min = [-1.5, -3]
+max = [4, 4]
+
+initialSolution = generateRandomSolution(min, max)
+solution = initialSolution
+bestSolution = solution
+bestSolutionValue = calculateSolutionValue(bestSolution)
+
+printSolution('Initial solution', solution)
+
+for i in range(5):
+  solution = generateRandomSolution(min, max)
+  solutionValue = calculateSolutionValue(solution)
+  if solutionValue < bestSolutionValue:
+    bestSolution = solution
+    bestSolutionValue = solutionValue
+
+printSolution('Best solution', bestSolution)
