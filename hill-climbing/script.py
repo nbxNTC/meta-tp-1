b2b1dd5f8fd02832131ddeac8bb9966bf4f0afb3
Mario Sergio <marionbxcz@gmail.com>
2021-09-05 18:54:58 -0300

    fix: create hill-climbing function

--- hill-climbing/script.py
+++ hill-climbing/script.py
@@ -1,28 +1,45 @@
 import random
 import math
 
-def hill_climbing(objective, minArray, maxArray, maxFailedAttempts):
+def hill_climbing(objective, minArray, maxArray, tweakProbability, noiseSizes, maxFailedAttempts):
+  def generateRandomValue():
+    randomValue = 0
+    while randomValue == 0 or randomValue == 1:
+      randomValue = random.uniform(0, 1)
+    return randomValue
+
   def generateRandomSolution():
     randomSolution = []
     for i in range(len(minArray)):
-      randomSolution.append(random.uniform(minArray[i], maxArray[i]))
+      randomSolution.append(((maxArray[i] - minArray[i]) * generateRandomValue()) + minArray[i])
     return randomSolution
 
+  def tweakSolution(solution):
+    for i in range(len(solution)):
+      if random.uniform(0, 1) < tweakProbability:
+        operations = [-1, 1]
+        multiplier = operations[random.randint(0, 1)]
+
+        noise = abs(solution[i] * noiseSizes[i])
+        newValue = solution[i] + (multiplier * noise)
+
+        if newValue >= minArray[i] and newValue <= maxArray[i]:
+          solution[i] = newValue
+
   def printSolution(title, solution):
-    x, y = solution
-    print(title)
-    print('f(', x, ', ', y, ') = ', objective(solution))
+    print('Variables', title,': ', solution)
+    print('Objective value: ', objective(solution))
 
   solution = generateRandomSolution()
   bestSolution = solution
   bestObjectiveValue = objective(bestSolution)
 
-  printSolution('Initial solution', solution)
+  printSolution('initial solution', solution)
 
   failedAttempts = 0
 
   while (True):
-    solution = generateRandomSolution()
+    tweakSolution(solution)
     objectiveValue = objective(solution)
 
     if objectiveValue < bestObjectiveValue:
@@ -35,8 +52,12 @@ def hill_climbing(objective, minArray, maxArray, maxFailedAttempts):
       if failedAttempts == maxFailedAttempts:
         break
 
-  printSolution('Best solution', bestSolution)
+  printSolution('best solution', bestSolution)
   print()
+
+def objectiveTest(solution):
+  x = solution[0]
+  return math.pow(x, 2)
 
 def objectiveOne(solution):
   x, y = solution
@@ -46,24 +67,39 @@ def objectiveTwo(solution):
   x, y = solution
   return ((-1 * (y + 47)) * math.sin(math.sqrt(abs((x / 2) + y + 47)))) - (x * math.sin(math.sqrt(abs(x - y - 47))))
 
-maxFailedAttempts = 100000
+maxFailedAttempts = 10000000
+
+minArray = [0]
+maxArray = [10]
+tweakProbability = 1
+noiseSizes = [0.3]
+
+hill_climbing(objectiveTest, minArray, maxArray, tweakProbability, noiseSizes, maxFailedAttempts)
 
 minArray = [-1.5, -3]
 maxArray = [4, 4]
+tweakProbability = 1
+noiseSizes = [0.3, 0.3]
 
-hill_climbing(objectiveOne, minArray, maxArray, maxFailedAttempts)
+hill_climbing(objectiveOne, minArray, maxArray, tweakProbability, noiseSizes, maxFailedAttempts)
 
 minArray = [-512, -512]
 maxArray = [512, 512]
+tweakProbability = 1
+noiseSizes = [0.3, 0.3]
 
-hill_climbing(objectiveOne, minArray, maxArray, maxFailedAttempts)
+hill_climbing(objectiveOne, minArray, maxArray, tweakProbability, noiseSizes, maxFailedAttempts)
 
 minArray = [-1, -2]
 maxArray = [0, -1]
+tweakProbability = 1
+noiseSizes = [0.3, 0.3]
 
-hill_climbing(objectiveOne, minArray, maxArray, maxFailedAttempts)
+hill_climbing(objectiveOne, minArray, maxArray, tweakProbability, noiseSizes, maxFailedAttempts)
 
 minArray = [511, 404]
 maxArray = [512, 405]
+tweakProbability = 1
+noiseSizes = [0.3, 0.3]
 
-hill_climbing(objectiveOne, minArray, maxArray, maxFailedAttempts)
+hill_climbing(objectiveOne, minArray, maxArray, tweakProbability, noiseSizes, maxFailedAttempts)
